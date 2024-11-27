416830080f54105a8648e13c195a9d8b8d286da3
Bogdan Bošković <bogdano@users.noreply.github.com>
2024-11-27 15:44:10 -0500

    cloudflare integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: 'hybrid',
+  adapter: cloudflare()
 });
