9e9b135ba1c8b926a760d69a704e38f0eb68b117
Bogdan Bošković <bogdano@users.noreply.github.com>
2024-12-08 13:58:56 -0500

    fixing cloudflare deployment error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import sitemap from '@astrojs/sitemap';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   output: 'hybrid',
   adapter: cloudflare(),
   prefetch: true
