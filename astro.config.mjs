08a1673be0f9487165e482f72252f663c1b2b706
Bogdan Bošković <bogdano@users.noreply.github.com>
2024-12-12 11:43:24 -0500

    test without CF adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
-  adapter: cloudflare(),
+
   prefetch: true
 });
