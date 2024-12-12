0d04f448d07aaac6969f21232936780d03458d4e
Bogdan Bošković <bogdano@users.noreply.github.com>
2024-12-12 12:19:19 -0500

    reverting changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-
+  output: 'static',
+  adapter: cloudflare(),
   prefetch: true
 });
