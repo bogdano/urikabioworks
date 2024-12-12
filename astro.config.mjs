2f5f64e097e90f0970fc676a2193430b3a005489
Bogdan Bošković <bogdano@users.noreply.github.com>
2024-12-12 11:13:10 -0500

    testing server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
+  output: 'server',
   adapter: cloudflare(),
   prefetch: true
 });
