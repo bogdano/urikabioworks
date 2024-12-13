9605688391b72b46b7bd6801847012949b368ef5
Bogdan Bošković <bogdano@users.noreply.github.com>
2024-12-13 11:31:03 -0500

    testing SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from '@astrojs/cloudflare';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  adapter: cloudflare(),
+  adapter: netlify(),
   prefetch: true
 });
