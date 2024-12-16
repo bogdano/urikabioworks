330ceaed0536d176f7d92b49117ac99ac02fe4d3
Bogdan Bošković <bogdano@users.noreply.github.com>
2024-12-16 16:22:13 -0500

    SEO, analytics, partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
+
+import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static',
+  site: "https://urikabioworks.com",
+  integrations: [
+    tailwind(),
+    partytown(),
+    sitemap({
+      filter: (page) => page !== "https://urikabioworks.com/thank-you",
+    }),
+  ],
+  output: "static",
   adapter: netlify(),
-  prefetch: true
+  prefetch: true,
 });
