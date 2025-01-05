f4763a28b7762ca5bb5bcd7adb71c3293925ed6c
Bogdan Bošković <bogdano@users.noreply.github.com>
2025-01-04 22:31:14 -0500

    publication page

--- src/components/PublicationsForm.svelte
+++ src/components/PublicationsForm.svelte
@@ -5,17 +5,19 @@ import { fade } from 'svelte/transition';
 import { directusFetch } from '../../lib/directusFetch.ts';
 const client = createDirectus('https://admin.urikabioworks.com').with(authentication()).with(rest());
 
+let { publication_id, publication_title } = $props();
+
 // "Global" state that determines which form to show
-let step = '';
+let step = $state('');
 
 let userId = '';
-let email = '';
-let firstName = '';
-let lastName = '';
-let company = '';
-let title = '';
-let message = '';
-let otp = '';
+let email = $state('');
+let firstName = $state('');
+let lastName = $state('');
+let company = $state('');
+let title = $state('');
+let message = $state('');
+let otp = $state('');
 let accessToken = '';
 let refreshToken = '';
 let errorMessage = '';
@@ -44,22 +46,22 @@ onMount(async () => {
 
 async function handleEmailSubmit() {
     try {
-      const res = await directusFetch(
-          'https://admin.urikabioworks.com/flows/trigger/8dccc9f3-e88e-4d6f-be2b-85ec39d4a9d6',
-          {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ email }),
-          }
-      );
-      const data = await res.json();
-      if (data.user_exists) {
-          step = 'otp';
-          userId = data.id;
-          alertMessage = 'We already have your email address on file.';
-      } else {
-          step = 'registration';
-      }
+        const res = await directusFetch(
+            'https://admin.urikabioworks.com/flows/trigger/8dccc9f3-e88e-4d6f-be2b-85ec39d4a9d6',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email }),
+            }
+        );
+        const data = await res.json();
+        if (data.user_exists) {
+            step = 'otp';
+            userId = data.id;
+            alertMessage = 'We already have your email address on file.';
+        } else {
+            step = 'registration';
+        }
     } catch (err) {
         console.error('Error in handleEmailSubmit:', err);
         errorMessage = 'Failed to submit email.';
@@ -81,6 +83,7 @@ async function handleRegistrationSubmit() {
                     title,
                     message,
                     source: 'publications',
+                    publication: publication_id,
                 }),
             }
         );
@@ -106,93 +109,128 @@ async function handleOtpSubmit() {
             body: JSON.stringify({ userId, otp }),
         });
         const response = await res.json();
-        if (response.refresh_token) {
+        if (response?.refresh_token) {
             accessToken = response.access_token;
-            refreshToken = response.refresh_token;
+            refreshToken = response.refresh_token || '';
             localStorage.setItem('access_token', accessToken);
             localStorage.setItem('refresh_token', refreshToken);
 
-            // Fetch user data
-            const userRes = await directusFetch('https://admin.urikabioworks.com/users/me?fields=first_name,last_name,email');
-            const userData = await userRes.json();
+            // fetch user data and leads data in parallel
+            const [userRes, leadRes] = await Promise.all([
+                directusFetch('https://admin.urikabioworks.com/users/me?fields=first_name,last_name,email'),
+                directusFetch(`https://admin.urikabioworks.com/items/leads?filter[user][_eq]=${userId}`)
+            ]);
+            if (!userRes.ok || !leadRes.ok) {
+                throw new Error('Failed to fetch user or leads data');
+            }
+            const [userData, leadsData] = await Promise.all([userRes.json(), leadRes.json()]);
             const user = userData.data;
+            const lead = leadsData.data?.[0];
             if (user) {
                 firstName = user.first_name;
                 lastName = user.last_name;
                 email = user.email;
-                localStorage.setItem('first_name', firstName);
-                localStorage.setItem('last_name', lastName);
-                localStorage.setItem('email', email);
+                localStorage.setItem('first_name', firstName || '');
+                localStorage.setItem('last_name', lastName || '');
+                localStorage.setItem('email', email || '');
             }
+            if (lead) {
+                company = lead.company;
+                title = lead.title;
+                localStorage.setItem('company', company || '');
+                localStorage.setItem('title', title || '');
+            }
+
+            // trigger Lead activation webhook
+            const flowRes = await directusFetch('https://admin.urikabioworks.com/flows/trigger/fb1a39c5-5c13-477c-bfbe-3baf4222d079', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ userId }),
+            });
+            if (!flowRes.ok) {
+                throw new Error('Failed to trigger Lead activation webhook');
+            }
+            message = '';
             alertMessage = "You've logged in.";
             step = 'logged-in';
         } else {
             throw new Error(response.message);
         }
     } catch (err) {
-      console.error('Error in handleOtpSubmit:', err);
-      errorMessage = 'Failed to verify OTP.';
+        console.error('Error in handleOtpSubmit:', err);
+        errorMessage = 'Failed to verify OTP.';
     }
 }
 
 async function handleFileDownload() {
     try {
-        const fileId = 'aa74b1ea-65d2-41ab-bd84-e321e0886747';
-        const res = await directusFetch(`https://admin.urikabioworks.com/assets/${fileId}`)
-        if (!res.ok) throw new Error('Failed to fetch file');
+        const paper_id = await directusFetch(`https://admin.urikabioworks.com/items/publications?filter[id][_eq]=${publication_id}`);
+        if (!paper_id.ok) throw new Error('Failed to fetch paper id');
+        const paper = await paper_id.json();
+        const fileId = paper.data[0].paper;
 
+        const res = await directusFetch(`https://admin.urikabioworks.com/assets/${fileId}`);
+        if (!res.ok) throw new Error('Failed to fetch file');
         const blob = await res.blob();
         const url = URL.createObjectURL(blob);
-
         // Open the file in a new tab
         window.open(url, '_blank');
-
         // Optionally revoke the URL after some time to free up memory
         setTimeout(() => URL.revokeObjectURL(url), 60000);
-        } catch (err) {
+    } catch (err) {
         console.error('Error fetching file:', err);
         errorMessage = 'Failed to download file.';
     }
 }
 </script>
 
-<div class="max-w-2xl mx-auto">
+<div class="min-w-96 mx-auto border border-zinc-200 dark:border-zinc-800 rounded-2xl px-8 py-4">
     {#if step === 'enter-email'}
     <form on:submit|preventDefault={handleEmailSubmit} in:fade={{ duration: 400 }}>
-        <h2>Access Publications</h2>
-        <p>Enter your email address to continue.</p>
-        <input autofocus class="textinput" type="email" bind:value={email} placeholder="you@example.com" autocomplete="on" />
+        <div class="flex flex-col justify-center my-4 text-center">
+            <h2 class="text-center font-bold text-xl">Access Publication</h2>
+            <p>Enter your email address to read the full document.</p>
+        </div>
+        <input autofocus class="textinput" type="email" bind:value={email} placeholder="your@address.com" autocomplete="on" />
         <button class="button" type="submit">Next</button>
     </form>
 
     {:else if step === 'registration'}
     <form on:submit|preventDefault={handleRegistrationSubmit} in:fade={{ duration: 400 }}>
-        <h2>Register</h2>
-        <input class="textinput" type="email" bind:value={email} placeholder="your@address.com *" />
-        <div class="flex flex-col md:flex-row gap-4">
-            <input class="textinput" type="text" bind:value={firstName} placeholder="First Name *" />
-            <input class="textinput" type="text" bind:value={lastName} placeholder="Last Name *" />
+        <div class="flex flex-col justify-center my-4 text-center">
+            <h2 class="text-center font-bold text-xl">Register</h2>
+            <p>Let us know who you are!</p>
         </div>
-        <div class="flex flex-col md:flex-row gap-4">
-            <input class="textinput" type="text" bind:value={company} placeholder="Company *" />
-            <input class="textinput" type="text" bind:value={title} placeholder="Title *" />
+        <input disabled class="textinput text-base" type="email" bind:value={email} placeholder="your@address.com *" />
+        <div class="flex flex-col md:flex-row md:gap-4">
+            <input autofocus class="textinput text-base" type="text" bind:value={firstName} placeholder="First Name *" />
+            <input class="textinput text-base" type="text" bind:value={lastName} placeholder="Last Name *" />
         </div>
-        <textarea class="textinput" bind:value={message} placeholder="Your introductory message..."></textarea>
+        <div class="flex flex-col md:flex-row md:gap-4">
+            <input class="textinput text-base" type="text" bind:value={company} placeholder="Company *" />
+            <input class="textinput text-base" type="text" bind:value={title} placeholder="Title *" />
+        </div>
+        <textarea class="textinput text-base" bind:value={message} placeholder="Your introductory message..."></textarea>
         <button class="button" type="submit">Register</button>
     </form>
 
     {:else if step === 'otp'}
     <form on:submit|preventDefault={handleOtpSubmit} in:fade={{ duration: 400 }}>
-        <h2>Check your email for your OTP</h2>
-        <input autofocus class="textinput" type="text" bind:value={otp} placeholder="Enter OTP code" />
-        <button class="button" type="submit">Submit OTP</button>
+        <div class="flex flex-col justify-center my-4 text-center">
+            <h2 class="text-center font-bold text-xl">Verify</h2>
+            <p>Please check your email for the <span class="font-bold text-urika-orange-400">verification code.</span></p>
+        </div>
+        <input autofocus class="textinput" type="text" bind:value={otp} placeholder="000000" />
+        <button class="button" type="submit">Verify</button>
     </form>
 
     {:else if step === 'logged-in'}
-    <div class="flex flex-col gap-2 text-center" in:fade={{ duration: 400 }}>
-        <h2>Success!</h2>
-        <p>You are logged in. Now you can access protected content.</p>
-        <button class="button" on:click={handleFileDownload}>ACCESS PAPER</button>
+    <div class="flex flex-col text-center" in:fade={{ duration: 400 }}>
+        <div class="flex flex-col justify-center my-4 text-center">
+            <h2 class="text-center font-bold text-xl">Thank you, {firstName}.</h2>
+            <p>Feel free to peruse our publications!</p>
+        </div>
+        <button class="button" on:click={handleFileDownload}>ACCESS THE PAPER</button>
     </div>
     {/if}
 </div>
